44be1f215522cca1d5f9d4b15c6163a71d6657ae
suhyun kim <ideptor@gmail.com>
2017-07-26 07:43:15 +0900

    problem 3.x stack

---
+++ src/p3_x_stack_queue/Stack.java
@@ -0,0 +1,25 @@
+package p3_x_stack_queue;
+
+import java.util.ArrayList;
+
+public class Stack <T extends Integer> {
+
+	ArrayList<T> stack=new ArrayList<>();
+	int top=-1;
+	
+	public void push(T elem) {
+		stack.add(elem);
+		top++;
+	}
+	public boolean empty() {
+		return (top>=0)?false:true;
+	}
+	public T pop() {
+		if(empty()) return null;
+		
+		T elem = stack.get(top);
+		top--;
+		return elem;
+	}
+
+}
---
+++ src/p3_x_stack_queue/TestStack.java
@@ -0,0 +1,37 @@
+package p3_x_stack_queue;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStack {
+
+	@Test
+	public void testStack() {
+		//given
+		int[] elem = {6,7,4,5,1,2,3};
+		
+		//when
+		Stack<Integer> stack = new Stack<>();
+		for(int e:elem) {
+			stack.push(e);
+		}
+		
+		// then
+		assertEquals(false, stack.empty());
+		assertEquals(new Integer(3), stack.pop());
+		assertEquals(new Integer(2), stack.pop());
+		assertEquals(new Integer(1), stack.pop());
+		assertEquals(new Integer(5), stack.pop());
+		assertEquals(new Integer(4), stack.pop());
+		assertEquals(new Integer(7), stack.pop());
+		assertEquals(new Integer(6), stack.pop());
+		assertNull(stack.pop());
+		assertEquals(true, stack.empty());
+	}
+	
+	
+	
+	
+
+}
