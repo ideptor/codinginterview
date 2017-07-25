ec07d46ad6e5428f7b7b2da660178ff2276354d6
suhyun kim <ideptor@gmail.com>
2017-07-26 08:41:43 +0900

    bugfix: stack.push: add->set, stack.pop: get -> remove,

--- src/p3_x_stack_queue/Stack.java
+++ src/p3_x_stack_queue/Stack.java
@@ -2,28 +2,25 @@ package p3_x_stack_queue;
 
 import java.util.ArrayList;
 
-public class Stack <T> {
+public class Stack <T extends Comparable<?>> {
 
 	ArrayList<T> stack=new ArrayList<>();
-	int top=-1;
 	
 	public void push(T elem) {
-		top++;
-		stack.set(top, elem);
+		stack.add(elem);
 	}
 	public boolean empty() {
-		return (top>=0)?false:true;
+		return (stack.size()<=0)?true:false;
 	}
 	public T pop() {
 		if(empty()) return null;
 		
-		T elem = stack.remove(top);
-		top--;
+		T elem = stack.remove(stack.size()-1);
 		return elem;
 	}
 	public T peek() {
 		if(empty()) return null;
-		return stack.get(top);
+		return stack.get(stack.size()-1);
 	}
 
 }
