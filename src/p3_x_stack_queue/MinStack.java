53498877d05667d3aeb62329d934d974df0278ef
suhyun kim <ideptor@gmail.com>
2017-07-26 08:44:30 +0900

    problem 3.3 min stack

--- src/p3_x_stack_queue/MinStack.java
+++ src/p3_x_stack_queue/MinStack.java
@@ -1,11 +1,25 @@
 package p3_x_stack_queue;
 
-public class MinStack<T> extends Stack<Integer> {
+public class MinStack<T extends Comparable<T>> extends Stack<T> {
 
 	Stack<T> minStack = new Stack<>();
-	public Object min() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public T min() {	
+		return minStack.peek();
+	}
+	
+	public void push(T elem) {
+		if(minStack.empty() || elem.compareTo(minStack.peek()) <= 0) {
+			minStack.push(elem);
+		}
+		super.push(elem);
 	}
 
+	public T pop() {
+		T elem = super.pop();
+		if(minStack.peek().equals(elem)) {
+			minStack.pop();
+		}
+		return elem;
+	}
 }
--- src/p3_x_stack_queue/TestMinStack.java
+++ src/p3_x_stack_queue/TestMinStack.java
@@ -1,6 +1,6 @@
 package p3_x_stack_queue;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
